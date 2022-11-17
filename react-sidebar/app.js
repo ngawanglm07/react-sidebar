a128c1434d8ab39725c382ef066cb8d15bbc7d16
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2022-11-17 12:47:57 +0530

    Add files via upload

---
+++ react-sidebar/app.js
@@ -0,0 +1,36 @@
+// import './App.css';
+// import Header from './components/Header';
+// import Content from './components/content';
+// import Footer from './components/footer';
+// import Card from './components/card';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import data from './data';
+
+// function App() {
+//    const cards = data.map(item => {
+//     return(
+//       <Card
+//         img = {item.img}
+//         name = {item.name}
+//         spa ={item.spa}
+//         paragraph ={item.paragraph}
+//       />
+//     )
+//    })
+
+//   return (
+//     <div className="App container">
+//       <Header />
+//       <Content />
+//       <section className="card-list">
+//       {cards}
+//       </section>
+      
+//       <Footer />
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
---
+++ react-sidebar/card.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// export default function Card(props){
+    
+//     return(
+//     <div class="card">
+//         <img src={props.img} className="card-image"></img>
+//         <div className="card-stats">
+//         <span > 5.0 </span>
+//         <span className="gray">  (6)</span>
+//         <span className="gray"> {props.spa} </span>
+//         </div>
+//         <p className="card-title">{props.name}</p>
+//         <p className="card-name"> <span className="bold">{props.paragraph}</span></p>
+//     </div>
+//  )
+// }
